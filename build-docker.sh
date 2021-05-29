8a94ffb64adf8ca4b0b9a166e1d5c9307d06d0a4
Marco Oesch <marco.oesch@decentage.com>
2021-05-29 13:37:22 +0200

    fixed build issue

--- build-docker.sh
+++ build-docker.sh
@@ -6,3 +6,6 @@ set -o nounset
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-peerexplorer-backend:${RELEASE_VERSION} .
 
+echo "Creating fake zip file to avoid artifact upload issue"
+mkdir -p ./build
+touch ./build/iep-peerexplorer-backend.zip
